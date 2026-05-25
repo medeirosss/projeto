96904db948c0427c0271f0f6ae61c3838351ce84
medeirosss <danielmedeiros66@gmail.com>
2026-05-24 22:16:29 -0300

    fixdofix again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,4 +12,4 @@ echo "Running database migrations..."
 alembic -c /app/alembic.ini upgrade heads
 
 echo "Starting Magi backend..."
-exec uvicorn main:app --host 0.0.0.0 --port 8443
+exec uvicorn backend.main:app --host 0.0.0.0 --port 8443
